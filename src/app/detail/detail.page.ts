8cb45d83cd5ee2eb87644211fbd6569d8b87bd34
kimtaemin <rlaxoals3456@gmail.com>
2022-06-03 17:53:48 +0900

    first commit

---
+++ src/app/detail/detail.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.page.html',
+  styleUrls: ['./detail.page.scss'],
+})
+export class DetailPage implements OnInit {
+
+  receivedData;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+
+    this.activatedRoute.queryParams.subscribe(params => {
+
+      if(this.router.getCurrentNavigation().extras.state){
+        this.receivedData = this.router.getCurrentNavigation().extras.state.movie;
+      }
+    })
+   }
+
+  ngOnInit() {
+  }
+
+}
